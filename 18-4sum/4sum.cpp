a7c69b5492d44882bf17ed87a4b106b921865c90
Abhishek kumar <singhabhishek.kiit@gmail.com>
2024-07-26 17:55:30 +0530

    Time: 26 ms (64.05%) | Memory: 16.2 MB (41.78%) - LeetSync

---
+++ 18-4sum/4sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> result;
+    int n = nums.size();
+    if (n < 4) return result; // not enough elements for a quadruplet
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < n - 3; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
+        for (int j = i + 1; j < n - 2; j++) {
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue; // skip duplicates
+            int left = j + 1, right = n - 1;
+            while (left < right) {
+                long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                if (sum == target) {
+                    result.push_back({nums[i], nums[j], nums[left], nums[right]});
+                    while (left < right && nums[left] == nums[left + 1]) left++; // skip duplicates
+                    while (left < right && nums[right] == nums[right - 1]) right--; // skip duplicates
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+    return result;
+}
+};
